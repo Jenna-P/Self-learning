79634c33b5f93272adb0c90f7628010e3ec8ab6c
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2022-04-13 17:10:22 +0200

    input reducer

--- React/redux/src/Components/Views.js
+++ React/redux/src/Components/Views.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { connect } from 'react-redux';
 import { addView } from '../redux/index'
 
 function Views({ count, addView}) {
+
+  const [number, setNumber] = useState(1);
   return (
     <div className="item">
       <h3>Views: {count}  </h3>
-      <button onClick={()=>  addView()}>Add View</button>
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)}/>
+      <button onClick={()=>  addView(number)}>Add View</button>
     </div>
   )
 }
@@ -25,7 +28,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = {   //pass object
     // addSubscriber: addSubscriber   -> es6 프로퍼티와 밸류 값이 같으면 하나만 써도 됨
-    addView
+    addView: (number) => addView(number)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Views)
